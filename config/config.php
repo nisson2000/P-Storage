58d1574ed6f64b5cd4b96ea03ece53eda293bf4a
root <root@0414test.nmlab.local>
2025-06-23 07:37:51 +0000

    second push

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oco2chgsu11g',
+  'passwordsalt' => 'CmLngt1i9llg4ncVsvjPPpX5Gb63B/',
+  'secret' => 'wQZjgZqLE8IX3ARyCS6NvHQn2ewyX8WJ+pYOFaCRkFq/rNmK',
+  'trusted_domains' => 
+  array (
+    0 => '0414test.nmlab.local',
+  ),
+  'datadirectory' => '/var/www/0414test.nmlab.local/data',
+  'dbtype' => 'mysql',
+  'version' => '28.0.14.1',
+  'overwrite.cli.url' => 'http://0414test.nmlab.local',
+  'dbname' => 'nextcloud_db',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud_db_admin',
+  'dbpassword' => 'nmlab504',
+  'installed' => true,
+);
