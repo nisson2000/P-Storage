341de44fb0ca44761b5195c41f7bfda14da6316a
root <root@PStorage.ntuee.temp>
2025-07-14 12:56:02 +0000

    0714_2

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc4topogunye',
+  'passwordsalt' => 'YvnfGMmYe/f24FotWKqLwMnF9NllsV',
+  'secret' => 'Q08IR/hq3cnzljtbXiosZc/mvh/MH+U4d59scoRC8rYWoVI4',
+  'trusted_domains' => 
+  array (
+    0 => 'pstorage.ntuee.temp',
+  ),
+  'datadirectory' => '/var/www/PStorage.ntuee.temp/data',
+  'dbtype' => 'mysql',
+  'version' => '28.0.14.1',
+  'overwrite.cli.url' => 'http://pstorage.ntuee.temp',
+  'dbname' => 'PStorage_db',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'PStorage_db_admin',
+  'dbpassword' => 'PStorage',
+  'installed' => true,
+);
